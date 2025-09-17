3766860c9d876b74271614d63299b0a6a823e66c
0hj1hyeon <128037333+0hj1hyeon@users.noreply.github.com>
2025-09-17 12:48:08 +0900

    F c

---
+++ CouDDa/src/main/java/com/example/CouDDa/CouDDaApplication.java
@@ -0,0 +1,13 @@
+package com.example.CouDDa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouDDaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouDDaApplication.class, args);
+	}
+
+}
---
+++ CouDDa/src/test/java/com/example/CouDDa/CouDDaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CouDDa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouDDaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
